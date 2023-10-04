56c36450fc9739c23b52a806ad9db21a2cca55e6
Bruno199006 <brandabruno9@gmail.com>
2023-10-04 16:33:17 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,30 @@
+let edad = Number(prompt("edad para ingresar a la discoteca?"));
+let capacidadMaxima = 100; 
+let cantidadPersonas = 0; 
+
+
+function controlDeEdad(){
+if(edad >= 18){
+    alert("Podes entrar a la discoteca");
+    
+}else{
+    alert("Lo siento eres menor de edad por lo tanto no puedes pasar");
+}
+}
+
+
+for (let cantidadPersonas = 0; cantidadPersonas < capacidadMaxima; cantidadPersonas ++) {
+    
+  let personasIngresan = Math.floor(Math.random() * 10); 
+
+  cantidadPersonas += personasIngresan; 
+
+  console.log("Total de personas en la discoteca: " + cantidadPersonas);
+
+  if (cantidadPersonas >= capacidadMaxima) {
+    console.log("La discoteca está llena. No pueden pasar más personas.");
+    break;
+}
+}
+controlDeEdad();
+
